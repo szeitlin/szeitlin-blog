8a1a154abc6d256f65e0eeb2704d49f9cdff4eec
Sam Zeitlin <samanthazeitlin@gmail.com>
2025-07-24 11:09:12 -0700

    fix path

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e   			#exit if any errors, so we don't publish garbage by accident
 set -x   			#print commands as you go, in case anything goes wrong
 hugo     			#build the public folder
 cd ../szeitlin.github.io
-cp -r ../szeitlin_blog/public/* .
+cp -r ../szeitlin-blog/public/* .
 git add *
 git commit -m "deploying latest changes"
 git push
